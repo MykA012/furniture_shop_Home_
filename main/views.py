fc506414fbceae2e516914b309b63bb012f7e1a3
Vova <aleshin.tomsk2@mail.ru>
2025-03-15 23:37:30 +0700

    try xd

--- main/views.py
+++ main/views.py
@@ -5,20 +5,21 @@ from goods.models import Categories
 
 
 def index(request):
-    categories = Categories.objects.all()
+    
     context = {
         'title': 'Home - главная',
         'content': 'Магазин мебели Home',
-        "categories": categories,
     }
 
     return render(request, 'main/index.html', context)
 
 
 def about(request):
+    
     context = {
         'title': 'Home - О нас', 
         'content': 'О нас',
         'text_on_page': 'Текст о том какой наш магазин хороший'
     }
+    
     return render(request, 'main/about.html', context)
